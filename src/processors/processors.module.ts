54f90747b1add65af43cb33bfd8d9404c5edc944
Junior Pezantes <werras04@gmail.com>
2026-06-30 11:40:10 -0500

    processors

--- src/processors/processors.module.ts
+++ src/processors/processors.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ChannelsModule } from '../channels/channels.module';
 import { NotificationProcessor } from './notification.processor';
 
 @Module({
+  imports: [ChannelsModule],
   providers: [NotificationProcessor],
 })
 export class ProcessorsModule {}
